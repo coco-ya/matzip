cdcadbfee05e199fd262593f957bf4cc6f1c7319
cocoya <guswl049012@naver.com>
2023-03-07 21:04:25 +0900

    03.07

--- src/main/java/dev/cocoya/matzip/MatzipApplication.java
+++ src/main/java/dev/cocoya/matzip/MatzipApplication.java
@@ -2,12 +2,19 @@ package dev.cocoya.matzip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MatzipApplication {
+public class MatzipApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MatzipApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MatzipApplication.class);
     }
 
 }
